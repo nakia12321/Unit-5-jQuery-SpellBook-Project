37c7f3fbcc315867a4638d72d6b645f2dec380fc
nakia12321 <43543837+nakia12321@users.noreply.github.com>
2019-03-20 10:25:33 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$(".button1").click(function(){
+ $("#spongebob").fadeToggle();
+});
+
+
+
+//Spell #2
+$(".button2").click(function(){
+ $(".show").toggle();
+});
+
+
+
+//Spell #3
+
+$(".button3 ").click(function(){
+ $(".slideup").slideUp();
+});
+
+//Spell #4
+$(".button4" ).click(function(){
+    $(".hide").hide();
+});
+
